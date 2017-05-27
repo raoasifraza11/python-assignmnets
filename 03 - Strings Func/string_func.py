a9ddebd90955078e8dfc954ee6e3b4789cc204a0
raoasifraza11 <raoasifraza1@hotmail.com>
2017-05-28 04:08:27 +0500

    Added Assignment No 3, not yet completed

---
+++ 03 - Strings Func/string_func.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# -*-  This Assignments implements 40 builtin str func() -*-
+# Encoding schema https://www.python.org/dev/peps/pep-0263
+
+""" ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
+                 1 
+    ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
+"""
+
+print("▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 1 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+greeting = "hello, how are you?!!!"
+
+print "Capitalize func(): ", greeting.capitalize()
+
+
+""" ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
+                 2
+    ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
+"""
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 2 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+str = "this is string example....wow!!!";
+
+print "str.center(50, 'a') : ", str.center(50, 'a')
+
