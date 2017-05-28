3f805126cbc3295f38377987c56d775a6c4377f1
raoasifraza11 <raoasifraza1@hotmail.com>
2017-05-28 15:01:19 +0500

    Complete 40 string examples

--- 03 - Strings Func/string_func.py
+++ 03 - Strings Func/string_func.py
@@ -20,7 +20,236 @@ print "Capitalize func(): ", greeting.capitalize()
 """
 
 print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 2 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
-str = "this is string example....wow!!!";
+myString = "this is string example....wow!!!" 
 
-print "str.center(50, 'a') : ", str.center(50, 'a')
+# Returns a space-padded string with the original string centered to a total of width columns.
+print "str.center(50, 'a') : ", myString.center(50, 'a')
+
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 3 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+secondString = "this is string example....wow!!!" 
+
+sub = "i" 
+print "count(sub, 4, 40) : ", secondString.count(sub, 4, 40)
+sub = "wow" 
+print "count(sub) : ", secondString.count(sub)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 4 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+thirdString = "this is string example....wow!!!"
+thirdString = thirdString.encode('base64', 'strict')
+
+print "Encoded String: " + thirdString
+print "Decoded String: " + thirdString.decode('base64', 'strict')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 5 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+print "Encoded String: " + thirdString.encode('base64','strict')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 6 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+print "Encoded String: " + thirdString.decode('base64','strict')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 7 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+forthString = "this is string example....wow!!!"
+
+print forthString
+suffix = "wow!!!" 
+print forthString.endswith(suffix) , ":: endwith(suffix)->" ,suffix
+print forthString.endswith(suffix,20) , ":: endwith(suffix, 20)->" ,suffix
+
+suffix = "is" 
+print forthString.endswith(suffix, 2, 4) , ":: endwith(2, 4)->" ,suffix
+print forthString.endswith(suffix, 2, 6) , ":: endwith(2, 6)->" ,suffix
+
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 8 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+fifthString = "this is\tstring example....wow!!!" 
+
+
+print "Original string: " + fifthString
+print "Defualt exapanded tab: " + fifthString.expandtabs()
+print "Double exapanded tab: " + fifthString.expandtabs(16)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 9 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+inputString = "this is string example....wow!!!" 
+searchingQuery = "exam" 
+print "string:: ", inputString
+print "Query:: ", searchingQuery
+print inputString.find(searchingQuery), "index"
+print inputString.find(searchingQuery, 10), "index"
+print inputString.find(searchingQuery, 40), "index mean not found"
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 10 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print "return index"
+print inputString.index(searchingQuery)
+print inputString.index(searchingQuery, 10)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 11 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+numberTest = "this2009"
+print numberTest.isalnum()
+
+alphastring = "this is string example....wow!!!"
+print alphastring.isalnum()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 12 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+alphastirngexample = "thisisstring"
+print alphastirngexample.isalpha()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 13 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+a = "124"
+print a.isdigit()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 14 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print alphastirngexample.islower()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 15 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print alphastirngexample.isupper()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 16 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+alphastirngexample = u"this0234"        # litral string
+print alphastirngexample.isnumeric()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 17 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+alphastirngexample = u"this0234"        # litral string
+alphastirngexample1 = u"  "        # litral string
+print alphastirngexample.isspace()
+print alphastirngexample1.isspace()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 18 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+alphastirngexample = u"this0234"        # litral string
+print alphastirngexample.istitle()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 19 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+s = "-" 
+seq = ("a", "b", "c")  # This is sequence of strings.
+print s.join( seq )
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 20 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+lenghtstring = "this is string example....wow!!!" 
+
+print "Length of the string: ", len(lenghtstring)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 21 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+lenghtstring = "this is string example....wow!!!" 
+
+print lenghtstring.ljust(50, '0')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 22 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+lenghtstring = "this is string example....wow!!!" 
+
+print lenghtstring.upper()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 23 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+lenghtstring = "this is string example....wow!!!" 
+
+print lenghtstring.lower()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 24 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print lenghtstring.lstrip()
+updatestr = "88888888this is string example....wow!!!8888888" 
+print updatestr.lstrip('8')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 25 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+from string import maketrans   # Required to call maketrans function.
+
+intab = "aeiou"
+outtab = "12345"
+trantab = maketrans(intab, outtab)
+
+mystring = "this is string example....wow!!!"
+print mystring.translate(trantab)
+
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 26 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print "Max character: " + max(mystring)
+
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 27 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+print "Max character: " + min("this-is-real-string-example....wow!!!")
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 28 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+replaceStr = "this is string example....wow!!! this is really string"
+print replaceStr.replace("is", "was")
+print replaceStr.replace("is", "was", 3)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 29 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+string1 = "this is really a string example....wow!!!" 
+string2 = "is" 
+
+print string1.rfind(string2)
+
+print string1.rfind(string2, 0, 10)
+print string1.rfind(string2, 10, 0)
+
+print string1.find(string2)
+print string1.find(string2, 0, 10)
+print string1.find(string2, 10, 0)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 30 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+string1 = "this is really a string example....wow!!!"
+string2 = "is"
+
+print string1.rindex(string2)
+print string1.index(string2)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 31 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+string1 = "this is really a string example....wow!!!"
+print string1.rjust(50, '0')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 32 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+string1 = "this is really a string example....wow!!!"
+print string1.rstrip()
+string2 = "88888888this is string example....wow!!!8888888"
+print string2.rstrip('8')
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 33 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+splitstringEx = "Line1-abcdef \nLine2-abc \nLine4-abcd"
+print splitstringEx.split()
+print splitstringEx.split(' ', 1)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 34 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+linestring = "Line1-a b c d e f\nLine2- a b c\n\nLine4- a b c d"
+print linestring.splitlines()
+print linestring.splitlines(0)
+print linestring.splitlines(3)
+print linestring.splitlines(1)
+print linestring.splitlines(4)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 35 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+linestring = "this is the world"
+print linestring.startswith( 'this' )
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 36 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+linestring = "this is the world"
+print linestring.swapcase()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 37 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+linestring = "this is the world"
+print linestring.title()
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 38 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+from string import maketrans   # Required to call maketrans function.
+
+intab = "aeiou"
+outtab = "12345"
+trantab = maketrans(intab, outtab)
+
+trans = "this is string example....wow!!!"
+print trans.translate(trantab)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 39 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+trans = "this is string example....wow!!!"
+print trans.zfill(40)
+print trans.zfill(50)
+
+print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 40 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+trans = u"this is string example....wow!!!"
+print trans.isdecimal()
+
+
+
+
+
 
