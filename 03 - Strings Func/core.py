ad9891fe23445fbd0745c019a19ebd588597fb7b
raoasifraza11 <raoasifraza1@hotmail.com>
2017-05-28 16:44:09 +0500

    Refactrization

---
+++ 03 - Strings Func/core.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# -*-  This Assignments implements 40 builtin str func() -*-
+# Encoding schema https://www.python.org/dev/peps/pep-0263
+# This is ilustrate func
+
+def feture_requested(number):
+    print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ %s ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬" %(number))
--- 03 - Strings Func/string_func.py
+++ 03 - Strings Func/string_func.py
@@ -2,32 +2,28 @@
 # -*- coding: latin-1 -*-
 # -*-  This Assignments implements 40 builtin str func() -*-
 # Encoding schema https://www.python.org/dev/peps/pep-0263
-
 """ ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
                  1 
     ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
 """
+from core import feture_requested  ## imported function form file
+import traceback
 
-print("▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 1 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+
+feture_requested("1")
+
 greeting = "hello, how are you?!!!"
+print str.capitalize.__doc__, "\n" ,greeting.capitalize()
 
-print "Capitalize func(): ", greeting.capitalize()
 
-
-""" ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
-                 2
-    ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬
-"""
-
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 2 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("2")
 myString = "this is string example....wow!!!" 
 
 # Returns a space-padded string with the original string centered to a total of width columns.
-print "str.center(50, 'a') : ", myString.center(50, 'a')
+print str.center.__doc__, "\n", myString.center(50, 'a')
 
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 3 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
-
+feture_requested("3")
 secondString = "this is string example....wow!!!" 
 
 sub = "i" 
@@ -35,23 +31,20 @@ print "count(sub, 4, 40) : ", secondString.count(sub, 4, 40)
 sub = "wow" 
 print "count(sub) : ", secondString.count(sub)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 4 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
-
+feture_requested("4")
 thirdString = "this is string example....wow!!!"
 thirdString = thirdString.encode('base64', 'strict')
 
 print "Encoded String: " + thirdString
 print "Decoded String: " + thirdString.decode('base64', 'strict')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 5 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
-
+feture_requested("5")
 print "Encoded String: " + thirdString.encode('base64','strict')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 6 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
-
+feture_requested("6")
 print "Encoded String: " + thirdString.decode('base64','strict')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 7 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("7")
 forthString = "this is string example....wow!!!"
 
 print forthString
@@ -64,7 +57,7 @@ print forthString.endswith(suffix, 2, 4) , ":: endwith(2, 4)->" ,suffix
 print forthString.endswith(suffix, 2, 6) , ":: endwith(2, 6)->" ,suffix
 
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 8 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("8")
 fifthString = "this is\tstring example....wow!!!" 
 
 
@@ -72,7 +65,7 @@ print "Original string: " + fifthString
 print "Defualt exapanded tab: " + fifthString.expandtabs()
 print "Double exapanded tab: " + fifthString.expandtabs(16)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 9 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("9")
 inputString = "this is string example....wow!!!" 
 searchingQuery = "exam" 
 print "string:: ", inputString
@@ -81,77 +74,77 @@ print inputString.find(searchingQuery), "index"
 print inputString.find(searchingQuery, 10), "index"
 print inputString.find(searchingQuery, 40), "index mean not found"
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 10 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("10")
 print "return index"
 print inputString.index(searchingQuery)
 print inputString.index(searchingQuery, 10)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 11 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("11")
 numberTest = "this2009"
 print numberTest.isalnum()
 
 alphastring = "this is string example....wow!!!"
 print alphastring.isalnum()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 12 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("12")
 alphastirngexample = "thisisstring"
 print alphastirngexample.isalpha()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 13 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("13")
 a = "124"
 print a.isdigit()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 14 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("14")
 print alphastirngexample.islower()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 15 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("15")
 print alphastirngexample.isupper()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 16 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("16")
 alphastirngexample = u"this0234"        # litral string
 print alphastirngexample.isnumeric()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 17 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("17")
 alphastirngexample = u"this0234"        # litral string
 alphastirngexample1 = u"  "        # litral string
 print alphastirngexample.isspace()
 print alphastirngexample1.isspace()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 18 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("18")
 alphastirngexample = u"this0234"        # litral string
 print alphastirngexample.istitle()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 19 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("19")
 s = "-" 
 seq = ("a", "b", "c")  # This is sequence of strings.
 print s.join( seq )
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 20 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("20")
 lenghtstring = "this is string example....wow!!!" 
 
 print "Length of the string: ", len(lenghtstring)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 21 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("21")
 lenghtstring = "this is string example....wow!!!" 
 
 print lenghtstring.ljust(50, '0')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 22 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("22")
 lenghtstring = "this is string example....wow!!!" 
 
 print lenghtstring.upper()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 23 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("23")
 lenghtstring = "this is string example....wow!!!" 
 
 print lenghtstring.lower()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 24 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("24")
 print lenghtstring.lstrip()
 updatestr = "88888888this is string example....wow!!!8888888" 
 print updatestr.lstrip('8')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 25 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("25")
 from string import maketrans   # Required to call maketrans function.
 
 intab = "aeiou"
@@ -162,19 +155,19 @@ mystring = "this is string example....wow!!!"
 print mystring.translate(trantab)
 
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 26 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("26")
 print "Max character: " + max(mystring)
 
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 27 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("27")
 print "Max character: " + min("this-is-real-string-example....wow!!!")
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 28 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("28")
 replaceStr = "this is string example....wow!!! this is really string"
 print replaceStr.replace("is", "was")
 print replaceStr.replace("is", "was", 3)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 29 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("29")
 string1 = "this is really a string example....wow!!!" 
 string2 = "is" 
 
@@ -187,29 +180,29 @@ print string1.find(string2)
 print string1.find(string2, 0, 10)
 print string1.find(string2, 10, 0)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 30 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("30")
 string1 = "this is really a string example....wow!!!"
 string2 = "is"
 
 print string1.rindex(string2)
 print string1.index(string2)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 31 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("31")
 string1 = "this is really a string example....wow!!!"
 print string1.rjust(50, '0')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 32 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("32")
 string1 = "this is really a string example....wow!!!"
 print string1.rstrip()
 string2 = "88888888this is string example....wow!!!8888888"
 print string2.rstrip('8')
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 33 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("33")
 splitstringEx = "Line1-abcdef \nLine2-abc \nLine4-abcd"
 print splitstringEx.split()
 print splitstringEx.split(' ', 1)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 34 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("34")
 linestring = "Line1-a b c d e f\nLine2- a b c\n\nLine4- a b c d"
 print linestring.splitlines()
 print linestring.splitlines(0)
@@ -217,19 +210,19 @@ print linestring.splitlines(3)
 print linestring.splitlines(1)
 print linestring.splitlines(4)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 35 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("35")
 linestring = "this is the world"
 print linestring.startswith( 'this' )
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 36 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("36")
 linestring = "this is the world"
 print linestring.swapcase()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 37 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("37")
 linestring = "this is the world"
 print linestring.title()
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 38 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("38")
 from string import maketrans   # Required to call maketrans function.
 
 intab = "aeiou"
@@ -239,12 +232,12 @@ trantab = maketrans(intab, outtab)
 trans = "this is string example....wow!!!"
 print trans.translate(trantab)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 39 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("39")
 trans = "this is string example....wow!!!"
 print trans.zfill(40)
 print trans.zfill(50)
 
-print("\n▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬ 40 ▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬")
+feture_requested("40")
 trans = u"this is string example....wow!!!"
 print trans.isdecimal()
 
